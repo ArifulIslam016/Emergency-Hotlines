4035eb29f6f88b3a14a28ffd0a5475d530a10513
Ariful Islam <arifulq234@gmail.com>
2025-08-27 06:52:37 +0600

    completed coin functionality

--- index.js
+++ index.js
@@ -14,3 +14,30 @@ for (const heartbtn of heartbtns) {
     document.getElementById("copy-counter").innerText = copyCounter;
   });
 }
+// Call button functionality
+const callBtns = document.getElementsByClassName("call-btn");
+
+for (const callBtn of callBtns) {
+  callBtn.addEventListener("click", function (e) {
+    let callCoin = getNumberValue("call-coin");
+    if (callCoin < 20) {
+      alert("Insufficient Coin");
+      return;
+    }
+    const element = e.target.closest(".call-btn");
+
+    const callBtnParent = element.parentNode.parentNode;
+    // console.log(callBtnParent);
+    namenumContainer = [];
+    const namenumbers = callBtnParent.querySelectorAll("h1");
+    for (const namenumber of namenumbers) {
+      namenumContainer.push(namenumber);
+      //   console.log(namenumber);
+    }
+    const name = namenumContainer[0].innerText;
+    const num = namenumContainer[1].innerText;
+    alert("calling... " + name + " : " + num);
+    const newcallCoin = callCoin - 20;
+    document.getElementById("call-coin").innerText = newcallCoin;
+  });
+}
